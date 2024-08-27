3562e68edee1620127bfa02d29d0fb8a73ea98e7
Lin <44311026+fengshanglin@users.noreply.github.com>
2024-08-27 23:21:09 +0800

    Test tools
    Add  test functions to verify code if  it is correct

---
+++ pythonScript/ToolLib/testTool.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+def printMsg(msg):
+    print("***********Test output:**********",msg)
+
+def printInfo(msg):
+    print("----->",msg)
+
+def printWarning(msg):
+    print("-***->",msg)
+
+def printError(msg):
+    print("*****->",msg)
