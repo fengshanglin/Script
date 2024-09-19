78be30967856397e7423133bb9ca7a3638f4b71a
Lin <44311026+fengshanglin@users.noreply.github.com>
2024-09-19 21:24:37 +0800

    Update function of  test tool

--- pythonScript/packages/ToolLib/testTool.py
+++ pythonScript/packages/ToolLib/testTool.py
@@ -1,18 +1,40 @@
 # -*- coding: UTF-8 -*-
+import inspect
+
+current_frame = inspect.currentframe()
 
 def printMsg(*args):
     msgCon=""
+    caller_frame = inspect.getframeinfo(current_frame.f_back)
+    stack_lists = inspect.stack()
     if args:
         for msg in args:
             msgCon = msgCon + str(msg) + "\t"
+        # print(f"***********Test output {current_frame.f_code.co_name}:**********",msgCon)
+        print(f"***********MsgFunction functionName:{stack_lists[1].function}() lineNumber:{stack_lists[1].lineno}**********",msgCon)
 
-        print("***********Test output:**********",msgCon)
+def printInfo(*args):
+    msgCon=""
+    stack_lists = inspect.stack()
+    if args:
+        for msg in args:
+            msgCon = msgCon + str(msg) + "\t"
+        # print(f"***********Test output {current_frame.f_code.co_name}:**********",msgCon)
+        print(f"----->functionName:{stack_lists[1].function}() lineNumber:{stack_lists[1].lineno}<-----",msgCon)
 
-def printInfo(msg):
-    print("----->",msg)
-
-def printWarning(msg):
-    print("-***->",msg)
-
-def printError(msg):
-    print("*****->",msg)
+def printWarning(*args):
+    msgCon=""
+    stack_lists = inspect.stack()
+    if args:
+        for msg in args:
+            msgCon = msgCon + str(msg) + "\t"
+        # print(f"***********Test output {current_frame.f_code.co_name}:**********",msgCon)
+        print(f"-***->functionName:{stack_lists[1].function}() lineNumber:{stack_lists[1].lineno}<-***-",msgCon)
+def printError(*args):
+    msgCon=""
+    stack_lists = inspect.stack()
+    if args:
+        for msg in args:
+            msgCon = msgCon + str(msg) + "\t"
+        # print(f"***********Test output {current_frame.f_code.co_name}:**********",msgCon)
+        print(f"*****->functionName:{stack_lists[1].function}() lineNumber:{stack_lists[1].lineno}<-*****",msgCon)
